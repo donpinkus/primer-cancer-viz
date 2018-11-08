3e65c4a2128a15ca038d3decb6c29607f9dd4385
Don Pinkus <don.pinkus@gmail.com>
2018-11-08 03:24:34 -0800

    factor out content

--- src/CancerGraphic.js
+++ src/CancerGraphic.js
@@ -1,4 +1,5 @@
 import React from "react";
+import stepTextContent from "./StepTextContent";
 import Stepper from "./Stepper";
 import Chart from "./Chart";
 
@@ -13,48 +14,9 @@ class CancerGraphic extends React.Component {
 
   width = 945;
 
-  content = [
-    {
-      title: "No title, bubble chart view",
-      blurb:
-        "No blurb, the first step in the NY times viz was on the bubble chart view"
-    },
-    {
-      title: "Where Does Breast Cancer Kill?",
-      blurb:
-        "While women in highly developed countries have higher diagnosis rates of breast cancer, women in the developing world who have breast cancer are more likely to die from the disease."
-    },
-    {
-      title: "High Mortality in Africa",
-      blurb: `Many of the poorest countries are in Africa, where a high percentage of breast cancer cases result in death.
-
-Poverty and misinformation keep women from seeking medical help. Even those who receive a diagnosis earlier can face delays in treatment because of scarce resources and dysfunctional health systems.`,
-      activeHDI: "Low"
-    },
-    {
-      title: "Divide Among Mid-Level Nations",
-      blurb: `Among women in moderately developed countries, incidence is relatively low but mortality is high.
-
-An exception is China, where incidence and mortality have been lower.`,
-      activeHDI: "Medium"
-    },
-    {
-      title: "Wealthier Nations",
-      blurb: `Among more highly developed nations, Arab countries tend to have lower incidence but higher mortality.
-
-By comparison, the ratio of deaths to new cases is lower in Latin America.`,
-      activeHDI: "High"
-    },
-    {
-      title: "The Richest Countries",
-      blurb: `Incidence is high but mortality is low in the United States and many Western and Northern European countries, in part because of a history of widespread screening and access to treatment.`,
-      activeHDI: "Very High"
-    }
-  ];
-
   render() {
     const { step } = this.state;
-    const content = this.content[step - 1];
+    const content = stepTextContent[step - 1];
 
     return (
       <div className="CancerGraphic" style={{ width: this.width }}>
